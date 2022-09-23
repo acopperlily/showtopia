491b2df0ae1d2e227f6778733117e0e332034aaa
acopperlily <acopperlily@yahoo.com>
2022-09-23 11:31:25 -0400

    Add images and signup page

---
+++ controllers/shows.js
@@ -0,0 +1,11 @@
+const User = require('../models/User');
+
+module.exports = {
+  getDashboard: async (req, res) => {
+    try {
+      res.render('dashboard.ejs');
+    } catch(err) {
+      console.log(err);
+    }
+  }
+};
--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
-//const authController = require('../controllers/auth');
+const authController = require('../controllers/auth');
 const homeController = require('../controllers/home');
-//const showsController = require('../controllers/shows');
-//const { ensureAuth, ensureGuest } = require('../middleware/auth');
+const showsController = require('../controllers/shows');
+const auth = require('../middleware/auth');
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 // Main Routes
 router.get('/', homeController.getIndex);
+
+router.get('/dashboard', ensureAuth, showsController.getDashboard);
+
+router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
+router.get('/logout', authController.logout);
+
+router.get('/signup', authController.getSignup);
+router.post('/signup', authController.postSignup);
 
 module.exports = router;
